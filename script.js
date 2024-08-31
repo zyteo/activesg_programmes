0b4e8d2377874bcb961c93649f1dcebd6e72d4b6
ZY <zteo015@e.ntu.edu.sg>
2024-08-31 16:18:35 +0800

    build getvenues to obtain all the venue names and id

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const venuesList = [];
 const venues =
   "https://activesg.gov.sg/api/trpc/programme.getProgrammeVenues?input=%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D";
 
@@ -10,8 +11,15 @@ const getVenues = async () => {
   return data;
 };
 
-getVenues().then((data) => {
-  console.log(data);
-  // stringified data
-  console.log(JSON.stringify(data));
-});
+getVenues()
+  .then((data) => {
+    // console.log(JSON.stringify(data.result.data.json));
+    for (let i = 0; i < data.result.data.json.length; i++) {
+      console.log(data.result.data.json[i]);
+      // update venuesList array in venues.js and save file
+      venuesList.push(data.result.data.json[i]);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
